48ecffe3124ed0b3b1b0622d2e33d7b7fe7a5928
IrfanTriHandoko <trihandokoirfan@gmail.com>
2020-07-21 09:44:35 +0700

    UI Test

---
+++ SimpleTest/Classes/Home/View/HeaderCell.swift
@@ -0,0 +1,17 @@
+//
+//  BannerCell.swift
+//  SimpleTest
+//
+//  Created by IRFAN TRIHANDOKO on 19/02/20.
+//  Copyright © 2020 IRFAN TRIHANDOKO. All rights reserved.
+//
+
+import UIKit
+
+class HeaderCell: UICollectionViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+}
